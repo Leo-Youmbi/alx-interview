5480022efd0845df9dda09494360228eecb1d129
Leo-Youmbi <leoyoumbi83@gmail.com>
2024-01-04 14:01:46 +0100

    pascal triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,15 @@
+def pascal_triangle(n: int):
+    triangle = []
+    if n > 0:
+        for i in range(n):
+            if i == 0:
+                triangle.append([1])
+            elif i == 1:
+                triangle.append([1,1])
+            else:
+                prev_array = triangle[-1]
+                triangle.append([1])
+                for j in range(i-1):
+                    triangle[-1].append(prev_array[j] + prev_array[j+1])
+                triangle[-1].append(1)
+    return triangle
