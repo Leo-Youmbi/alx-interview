dd3f126728fa7f09dae0b8de5f0fdfa25727268d
Leo-Youmbi <leoyoumbi83@gmail.com>
2024-02-21 22:59:50 +0100

    console.log(person);

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
 
 const arrayOfArguments = process.argv.slice(2);
+
 if (arrayOfArguments.length === 1) {
   const arg = arrayOfArguments[0];
+
   const fetcher = async (url = '') => {
     try {
       const res = await fetch(url);
@@ -12,14 +14,17 @@ if (arrayOfArguments.length === 1) {
       return e.message;
     }
   };
-  fetcher(`https://swapi-api.alx-tools.com/api/films/${arg}/`)
-    .then((res) => {
-      const people = res.characters;
-      people.forEach((personUrl) => {
-        fetcher(personUrl)
-          .then((res) => console.log(res.name));
-      });
-    });
+
+  const getNames = async (arg) => {
+    const result = await fetcher(`https://swapi-api.alx-tools.com/api/films/${arg}/`);
+    const people = result.characters;
+    for (const personUrl of people) {
+      const res = await fetcher(personUrl);
+      console.log(res.name);
+    }
+  };
+
+  getNames(arg);
 } else {
   console.log('Usage: ./0-starwars_characters.js number');
 }
