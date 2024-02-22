dd6f5fe7ac869551d81408ce4b364169f5b100e5
Leo-Youmbi <leoyoumbi83@gmail.com>
2024-02-22 03:22:38 +0100

    Star Wars API

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,30 +1,53 @@
 #!/usr/bin/node
 
+const request = require('request');
+
 const arrayOfArguments = process.argv.slice(2);
 
 if (arrayOfArguments.length === 1) {
-  const arg = arrayOfArguments[0];
+  const movieId = arrayOfArguments[0];
 
-  const fetcher = async (url = '') => {
+  const getCharactersForMovie = (movieId) => {
+    return new Promise((resolve, reject) => {
+      const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+      request(url, (error, response, body) => {
+        if (error) {
+          reject(error);
+        } else {
+          const filmData = JSON.parse(body);
+          const characters = filmData.characters;
+          resolve(characters);
+        }
+      });
+    });
+  };
+
+  const fetchCharacterData = (characterUrl) => {
+    return new Promise((resolve, reject) => {
+      request(characterUrl, (error, response, body) => {
+        if (error) {
+          reject(error);
+        } else {
+          const characterData = JSON.parse(body);
+          resolve(characterData);
+        }
+      });
+    });
+  };
+
+  const printCharactersForMovie = async (movieId) => {
     try {
-      const res = await fetch(url);
-      const result = res.json();
-      return result;
-    } catch (e) {
-      return e.message;
+      const characters = await getCharactersForMovie(movieId);
+      for (const characterUrl of characters) {
+        const characterData = await fetchCharacterData(characterUrl);
+        console.log(characterData.name);
+      }
+    } catch (error) {
+      console.error('Error:', error);
     }
   };
 
-  const getNames = async (arg) => {
-    const result = await fetcher(`https://swapi-api.alx-tools.com/api/films/${arg}/`);
-    const people = result.characters;
-    for (const personUrl of people) {
-      const res = await fetcher(personUrl);
-      console.log(res.name);
-    }
-  };
-
-  getNames(arg);
+  printCharactersForMovie(movieId);
 } else {
   console.log('Usage: ./0-starwars_characters.js number');
 }
